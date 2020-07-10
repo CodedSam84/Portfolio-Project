2e1ebbca30183f2181e70b86b226b911854eddd3
samuel atobatele <swumatob@yahoo.co.uk>
2020-07-10 03:24:29 +0100

    created edit and update actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
   def index
     @portfolio_items = Portfolio.all
   end
@@ -16,12 +17,30 @@ class PortfoliosController < ApplicationController
         else
           format.html { render :new }
         end
-      end
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    respond_to do |format|
+        if @portfolio_item.update(portfolio_params)
+          format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+    end
   end
 
   private
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end

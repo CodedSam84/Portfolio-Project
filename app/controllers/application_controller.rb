a4a027dfbd5629b1d15ab19f2b95347def44e785
samuel atobatele <swumatob@yahoo.co.uk>
2020-07-22 01:24:40 +0100

    refactored current user concern implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SessionParameter
-
-  def current_user
-    super || guest_user
-  end
-
-  def guest_user
-    OpenStruct.new(name:"Guest User", first_name:"Guest", last_name:"User", email:"guest@yahoo.com")
-  end
+  include CurrentUserConcern
 end

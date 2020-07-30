0beba0cedad37a41c6fd29a55569a14dfd8c1b7d
samuel atobatele <swumatob@yahoo.co.uk>
2020-07-30 09:32:39 +0100

    Added footer to the application layout, updated pages view, and added styles and map to the contact page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   include SessionParameter
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = AtobateleViewTool::Renderer.copyright "Samuel Atobatele", "All rights reserved"
+  end
 end

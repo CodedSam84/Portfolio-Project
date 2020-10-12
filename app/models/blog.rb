2a1d8a83faab1b63d4e9449c7989e7ac277e661d
samuel atobatele <swumatob@yahoo.co.uk>
2020-10-12 14:00:09 +0100

    Added validations for topic in blogs and added a topic form element

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ApplicationRecord
   friendly_id :title, use: :slugged
   enum status: { draft: 0, published: 1 }
   
-  validates :title, :body, presence: true
+  validates :title, :body, :topic_id, presence: true
   belongs_to :topic
   has_many :comments, dependent: :destroy
 
